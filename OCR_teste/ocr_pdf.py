7564dec26d8bf4eafb9f9d84097e18c47263f194
GuilhermeCarvalho1144 <guilhermecarvalho1144@gmail.com>
2019-04-28 13:36:15 -0300

    Teste com primeiro CV

--- OCR_teste/ocr_pdf.py
+++ OCR_teste/ocr_pdf.py
@@ -1,10 +1,35 @@
-import pandas as pd
-import numpy as np 
 from PIL import Image
 import pytesseract
+import io
+from wand.image import Image as wi 
+
+
+'''
+##Teste com a imagem
 
 image = Image.open("sample1.jpg")
 
 text = pytesseract.image_to_string(image, lang = 'eng')
 
 print(text)
+'''
+
+##Teste com .pdf
+
+pdf = wi(filename = "CV_GuilhermeCPereira.pdf", resolution = 300)
+pdfImage = pdf.convert('jpeg')
+
+imageBlobs = []
+
+for img in pdfImage.sequence:
+	imgPage = wi(image = img)
+	imageBlobs.append(imgPage.make_blob('jpeg'))
+
+recognized_text = []
+
+for imgBlob in imageBlobs:
+	im = Image.open(io.BytesIO(imgBlob))
+	text = pytesseract.image_to_string(im, lang = 'eng')
+	recognized_text.append(text)
+
+print(recognized_text)
